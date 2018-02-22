2bb992f6784cbd6eee78bc11b2f8a4b77c473418
Yoon Young <yunyoung1819@hanmail.net>
2018-02-22 22:52:00 +0900

    자료형 숫자형

---
+++ exercise1.py
@@ -0,0 +1,36 @@
+# 자료형
+# 정수형 : 정수형(Integer)이란 말 그대로 정수를 뜻하는 자료형을 말한다.
+a=123
+print(a)
+
+a=-178
+print(a)
+
+a=0
+print(a)
+
+# 실수형: 파이썬에서 실수형(Floating-point)은 소수점이 포함된 숫자를 말한다.
+a=1.2
+print(a)
+
+a=-3.45
+print(a)
+
+#컴퓨터식 지수 표현 방식
+a=4.24E10
+print(a)
+
+a=4.24e-10
+print(a)
+
+# 8진수: 0o 또는 0O (숫자 0 + 알파벳 소문자 o 또는 O)로 시작
+a = 0o177
+print(a)
+
+# 16진수 : 0x로 시작
+a=0x8ff
+b=0xABC
+
+print(a)
+print(b)2
+.
