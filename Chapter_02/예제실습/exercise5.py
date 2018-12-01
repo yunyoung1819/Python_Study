aeb8a4c6b0165ba6d10f88847a9ade63c0953fcd
yunyoung1819 <yunyoung1819@hanmail.net>
2018-12-01 23:58:05 +0900

    Chapter2 practice code add

--- Chapter_02/예제실습/exercise5.py
+++ Chapter_02/예제실습/exercise5.py
@@ -3,3 +3,27 @@
 
 odd = [1,3,5,7,9]
 print(odd)
+
+# 여러가지 리스트
+a = []
+b = [1,2,3]
+c = ['Life', 'is', 'too', 'short']
+d = [1,2, 'Life', 'is']
+e = [1,2, ['Life', 'is']]
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+# 리스트의 인덱싱과 슬라이싱
+# 리스트도 문자열처럼 인덱싱과 슬라이싱이 가능하다
+
+# 리스트 인덱싱
+a = [1,2,3]
+print(a)
+
+# a[0]은 리스트 a의 첫번째 요소 값을 말한다
+print(a[0])
+print(a[0] + a[2]) # 1+3
